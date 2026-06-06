8894041cb5ad62f0a3b3cd004bc8a62cf76315b6
NIHHHHHHHHHHH <nihalgavandi00@gmail.com>
2026-06-06 13:47:17 +0530

    init: express server setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "tasko api running" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
